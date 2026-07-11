a99558b248893db79635902b33ef36f44e43c8f2
Yousef Mohamed <yousefmohammed867@gmail.com>
2026-07-11 20:53:51 +0300

    Create Task_stdyuype

---
+++ Task_stdtype/stdtype.c
@@ -0,0 +1,16 @@
+#ifndef TASKSTD_H
+#define TASKSTD_H
+
+typedef unsigned char U8_t;
+typedef signed char S8_t;
+typedef unsigned short int U16_t;
+typedef signed short int S16_t;
+typedef unsigned long int U32_t;
+typedef signed long int S32_t;
+typedef unsigned long long int U64_t;
+typedef signed long long int S64_t;
+typedef float F32_t;
+typedef double D64_t;
+typedef long double D128_t;
+
+#endif
